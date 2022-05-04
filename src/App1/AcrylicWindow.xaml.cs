1acb313345028c1705d6be55def6ac76df5d0b97
Lucimarck J S Dias <lucimarck.dias@outlook.com>
2022-05-03 21:57:48 -0400

    Add Mica and Acrylic window samples

---
+++ src/App1/AcrylicWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.UI.Xaml;
+using WinRT;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace App1;
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class AcrylicWindow : Window
+{
+    WindowsSystemDispatcherQueueHelper m_wsdqHelper; // See separate sample below for implementation
+    Microsoft.UI.Composition.SystemBackdrops.DesktopAcrylicController m_acrylicController;
+    Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration m_configurationSource;
+
+    public AcrylicWindow()
+    {
+        InitializeComponent();
+
+        TrySetAcrylicBackdrop();
+    }
+
+    private void myButton_Click(object sender, RoutedEventArgs e)
+    {
+        myButton.Content = "Clicked";
+    }
+
+    bool TrySetAcrylicBackdrop()
+    {
+        if (Microsoft.UI.Composition.SystemBackdrops.DesktopAcrylicController.IsSupported())
+        {
+            m_wsdqHelper = new WindowsSystemDispatcherQueueHelper();
+            m_wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
+
+            // Hooking up the policy object
+            m_configurationSource = new Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration();
+            this.Activated += Window_Activated;
+            this.Closed += Window_Closed;
+
+            // Initial configuration state.
+            m_configurationSource.IsInputActive = true;
+            switch (((FrameworkElement)this.Content).ActualTheme)
+            {
+                case ElementTheme.Dark: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Dark; break;
+                case ElementTheme.Light: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Light; break;
+                case ElementTheme.Default: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default; break;
+            }
+
+            if (Content is FrameworkElement { ActualTheme: var actualTheme })
+            {
+                m_configurationSource.Theme = actualTheme switch
+                {
+                    ElementTheme.Dark => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Dark,
+                    ElementTheme.Light => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Light,
+                    ElementTheme.Default => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default,
+                    _ => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default,
+                };
+            }
+
+            m_acrylicController = new Microsoft.UI.Composition.SystemBackdrops.DesktopAcrylicController();
+
+            // Enable the system backdrop.
+            // Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
+            m_acrylicController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
+            m_acrylicController.SetSystemBackdropConfiguration(m_configurationSource);
+            return true; // succeeded
+        }
+
+        return false; // Acrylic is not supported on this system
+    }
+    
+    private void Window_Activated(object sender, WindowActivatedEventArgs args)
+    {
+        m_configurationSource.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
+
+        if (Content is FrameworkElement frameworkElement)
+        {
+            frameworkElement.ActualThemeChanged += (sender, args) =>
+            {
+                m_configurationSource.Theme = frameworkElement.ActualTheme switch
+                {
+                    ElementTheme.Dark => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Dark,
+                    ElementTheme.Light => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Light,
+                    ElementTheme.Default => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default,
+                    _ => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default
+                };
+            };
+        };
+    }
+
+    private void Window_Closed(object sender, WindowEventArgs args)
+    {
+        // Make sure any Mica/Acrylic controller is disposed so it doesn't try to
+        // use this closed window.
+        if (m_acrylicController != null)
+        {
+            m_acrylicController.Dispose();
+            m_acrylicController = null;
+        }
+        this.Activated -= Window_Activated;
+        m_configurationSource = null;
+    }
+}
---
+++ src/App1/MicaWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.UI.Xaml;
+using WinRT;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace App1;
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MicaWindow : Window
+{
+    WindowsSystemDispatcherQueueHelper m_wsdqHelper; // See separate sample below for implementation
+    Microsoft.UI.Composition.SystemBackdrops.MicaController m_micaController;
+    Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration m_configurationSource;
+
+    public MicaWindow()
+    {
+        InitializeComponent();
+
+        TrySetMicaBackdrop();
+    }
+
+    private void myButton_Click(object sender, RoutedEventArgs e)
+    {
+        myButton.Content = "Clicked";
+    }
+
+    bool TrySetMicaBackdrop()
+    {
+        if (Microsoft.UI.Composition.SystemBackdrops.MicaController.IsSupported())
+        {
+            m_wsdqHelper ??= new WindowsSystemDispatcherQueueHelper();
+            m_wsdqHelper.EnsureWindowsSystemDispatcherQueueController();
+
+            // Hooking up the policy object
+            m_configurationSource = new Microsoft.UI.Composition.SystemBackdrops.SystemBackdropConfiguration();
+            Activated += Window_Activated;
+            Closed += Window_Closed;
+
+            // Initial configuration state.
+            m_configurationSource.IsInputActive = true;
+
+            if (Content is FrameworkElement { ActualTheme: var actualTheme })
+            {
+                m_configurationSource.Theme = actualTheme switch
+                {
+                    ElementTheme.Dark => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Dark,
+                    ElementTheme.Light => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Light,
+                    ElementTheme.Default => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default,
+                    _ => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default
+                };
+            }
+
+            m_micaController = new Microsoft.UI.Composition.SystemBackdrops.MicaController();
+
+            // Enable the system backdrop.
+            // Note: Be sure to have "using WinRT;" to support the Window.As<...>() call.
+            m_micaController.AddSystemBackdropTarget(this.As<Microsoft.UI.Composition.ICompositionSupportsSystemBackdrop>());
+            m_micaController.SetSystemBackdropConfiguration(m_configurationSource);
+            return true; // succeeded
+        }
+
+        return false; // Mica is not supported on this system
+    }
+
+    private void Window_Activated(object sender, WindowActivatedEventArgs args)
+    {
+        m_configurationSource.IsInputActive = args.WindowActivationState != WindowActivationState.Deactivated;
+
+        if (Content is FrameworkElement frameworkElement)
+        {
+            frameworkElement.ActualThemeChanged += (sender, args) =>
+            {
+                m_configurationSource.Theme = frameworkElement.ActualTheme switch
+                {
+                    ElementTheme.Dark => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Dark,
+                    ElementTheme.Light => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Light,
+                    ElementTheme.Default => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default,
+                    _ => Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default
+                };
+            };
+        };
+    }
+
+    private void Window_Closed(object sender, WindowEventArgs args)
+    {
+        // Make sure any Mica/Acrylic controller is disposed so it doesn't try to
+        // use this closed window.
+        if (m_micaController != null)
+        {
+            m_micaController.Dispose();
+            m_micaController = null;
+        }
+        this.Activated -= Window_Activated;
+        m_configurationSource = null;
+    }
+}
